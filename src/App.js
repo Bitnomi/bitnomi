3324fd2ee6aa525abd93c680e435f57671f09674
Harshil Agrawal <ghagrawal17@gmail.com>
2018-10-25 20:11:11 +0530

    dashboard page designed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import UserProfile from './components/UserProfile';
-
+import UserDashboard from './components/UserDashboard';
 
 export default class App extends Component {
   displayName = App.name
@@ -17,6 +17,7 @@ export default class App extends Component {
         <Route path='/counter' component={Counter} />
         <Route path='/fetchdata' component={FetchData} />
         <Route path='/userprofile' component={UserProfile} />
+        <Route path='/userdashboard' component={UserDashboard} />
       </Layout>
     );
   }
---
+++ src/components/DashboardTable.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Table, Button, Tag, Avatar } from 'antd';
+
+const columns = [{
+  title: 'Coin',
+  dataIndex: 'Coin',
+  key: 'coin',
+  render: text => (
+      <span>
+        <Avatar size='large' src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+        {text}
+      </span>
+  ),
+}, {
+  title: 'Available',
+  dataIndex: 'Available',
+  key: 'available',
+}, {
+  title: 'Limits',
+  dataIndex: 'Limits',
+  key: 'limits',
+}, {
+    title: 'Price',
+    dataIndex: 'Price',
+    key: 'price',
+},{
+  title: 'Payment Methods',
+  key: 'payment',
+  dataIndex: 'payment',
+  render: payment => (
+    <span>
+      {payment.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+    </span>
+  ),
+}, {
+  title: 'Control',
+  key: 'control',
+  render: (text, record) => (
+    <span>
+    <Button type="primary">Sell</Button>
+    </span>
+  ),
+}];
+
+const data = [{
+  key: '1',
+  Coin: 'HT',
+  Available: '5000 HT',
+  Limits: '500-5000 CNY',
+  Price: '11 CNY',
+  payment: ['BankCard'],
+}, {
+  key: '2',
+  Coin: 'EOS',
+  Available: '5000 EOS',
+  Limits: '500-5000 CNY',
+  Price:'36 CNY',
+  payment: ['BankCard'],
+}, {
+  key: '3',
+  Coin: 'HT',
+  Available: '5000 HT',
+  Limits: '500-5000 CNY',
+  Price:'13.6 CNY',
+  payment: ['BankCard'],
+}];
+
+function DashboardTable(){
+    return(
+        <Table columns={columns} dataSource={data} />
+    );
+}
+
+export default DashboardTable;
